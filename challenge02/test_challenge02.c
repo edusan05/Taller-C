31a47c84f6a79d1fbf1063ee03ad0fe07e900c0c
edusa <edusanviljob@gmail.com>
2025-11-05 14:58:09 +0100

    feat: challenge02 finalizado

---
+++ challenge02/test_challenge02.c
@@ -0,0 +1,50 @@
+#include <assert.h>
+#include <stdio.h>
+
+int suma(int a, int b)
+{
+    return a + b;
+}
+int resta(int a, int b)
+{
+    return a - b;
+}
+int multiplicacion(int a, int b)
+{
+    return a * b;
+}
+int division(int a, int b)
+{
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: División por cero.\n");
+        return 0; // Retorna 0 en caso de división por cero
+    }
+}
+
+// pruebas unitarias
+void test_Completo(){
+    assert(suma(2, 3) == 5);
+    assert(suma(-1, 1) == 0);
+    assert(suma(-2, -3) == -5);
+
+    assert(resta(5, 3) == 2);
+    assert(resta(2, 4) == -2);
+    assert(resta(-2, -3) == 1);
+
+    assert(multiplicacion(2, 3) == 6);
+    assert(multiplicacion(-2, 3) == -6);
+    assert(multiplicacion(-2, -3) == 6);
+
+    assert(division(6, 3) == 2);
+    assert(division(5, 2) == 2); // División entera
+    assert(division(-6, 3) == -2);
+
+    printf("Todas las pruebas pasaron.\n");
+}
+
+int main() {
+    test_Completo();
+    return 0;
+}
